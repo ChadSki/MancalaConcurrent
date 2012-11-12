7e8bda51775560905a938f6ec909c12047647931
Chad Zawistowski <chad.zawistowski@gmail.com>
2012-11-12 10:59:04 -0800

    Reorder imports

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,11 @@
-import ProducerConsumer.MancalaConsumer;
-import ProducerConsumer.MancalaProducer;
-import ProducerConsumer.MancalaQueue;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
+import ProducerConsumer.MancalaConsumer;
+import ProducerConsumer.MancalaProducer;
+import ProducerConsumer.MancalaQueue;
 
 /**
  * TODO javadocs
